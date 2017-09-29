8e66e220ea6317462d4158faaf7ca14cca94ed37
guimelcode <guillaume.melennec@gmail.com>
2017-09-29 11:40:31 +0200

    Manage display Événement 🤠

--- includes/expositions.php
+++ includes/expositions.php
@@ -19,51 +19,53 @@ function expositions(){
   echo "<div class='bxslider swiper-wrapper'>";
   $args = date_test()[0];
 
-	$ajax_query = new WP_Query($args);
+  $ajax_query = new WP_Query($args);
 
 
-	if ( $ajax_query->have_posts() ) : while ( $ajax_query->have_posts() ) : $ajax_query->the_post();
-    echo "<div class='a-venir swiper-slide'>";
-    get_template_part( 'template-parts/article_mini' );
-    $counter++;
-    echo "</div >";
-	endwhile;
-  endif;
+  if ( $ajax_query->have_posts() ) : while ( $ajax_query->have_posts() ) : $ajax_query->the_post();
+  echo "<div class='a-venir swiper-slide'>";
+  get_template_part( 'template-parts/article_mini' );
+  $counter++;
+  echo "</div >";
+endwhile;
+endif;
 
 
-  $args = date_test()[1];
+$args = date_test()[1];
 
-	$ajax_query = new WP_Query($args);
+$ajax_query = new WP_Query($args);
 
-	if ( $ajax_query->have_posts() ) : while ( $ajax_query->have_posts() ) : $ajax_query->the_post();
-    echo "<div class='en-cours swiper-slide'>";
-    get_template_part( 'template-parts/article' );
-    $counter++;
-    echo "</div>";
-	endwhile;
-  endif;
+if ( $ajax_query->have_posts() ) : while ( $ajax_query->have_posts() ) : $ajax_query->the_post();
+echo "<div class='en-cours swiper-slide'>";
+get_template_part( 'template-parts/article' );
+
+$counter++;
+
+echo "</div>";
+endwhile;
+endif;
 
 
-  $args = date_test()[2];
+$args = date_test()[2];
 
-	$ajax_query = new WP_Query($args);
+$ajax_query = new WP_Query($args);
 
-  echo "<div class='archives swiper-slide'>
-  <div class='archives-hover'><span class='close-tag'></span><div class='content'></div></div><article class='row col-xs-offset-3 col-xs-20 col-sm-offset-3 col-sm-23 col-lg-offset-1 col-lg-25'>";
-	if ( $ajax_query->have_posts() ) : while ( $ajax_query->have_posts() ) : $ajax_query->the_post();
+echo "<div class='archives swiper-slide'>
+<div class='archives-hover'><span class='close-tag'></span><div class='content'></div></div><article class='row col-xs-offset-3 col-xs-20 col-sm-offset-3 col-sm-23 col-lg-offset-1 col-lg-25'>";
+if ( $ajax_query->have_posts() ) : while ( $ajax_query->have_posts() ) : $ajax_query->the_post();
 
-    get_template_part( 'template-parts/cell' );
-    $counter++;
+get_template_part( 'template-parts/cell' );
+$counter++;
 // var_dump(get_the_title()) ;
 
-	endwhile;
-  endif;
-  echo "</article></div>";
+endwhile;
+endif;
+echo "</article></div>";
 
-  echo "</div></div>";
-  echo "<span class='process-count' count='".$counter."'></span>";
+echo "</div></div>";
+echo "<span class='process-count' count='".$counter."'></span>";
 
-  /**/
+/**/
 }
 
 function date_test(){
@@ -89,7 +91,7 @@ function date_test(){
   );
 
   $avenir = array(
-    'post_type'    => 'exposition',
+    'post_type'    => array('exposition', 'evenement'),
     'meta_query' => array(
       'relation' => 'AND',
       array(
@@ -108,21 +110,37 @@ function date_test(){
   );
 
   $enCours = array(
-    'post_type'    => 'exposition',
+    'post_type'    => array('exposition', 'evenement'),
     'meta_query' => array(
-      'relation' => 'AND',
+      'relation' => 'OR',
       array(
-        'key'     => 'date_debut_exposition',
-        'value'   => $today,
-        'compare' => '<=',
+        'relation' => 'AND',
+        array(
+          'key'     => 'date_debut_exposition',
+          'value'   => $today,
+          'compare' => '<=',
+        ),
+        array(
+          'key' => 'date_fin_exposition',
+          'value'   => $today,
+          'compare' => '>=',
+        ),
       ),
       array(
-        'key' => 'date_fin_exposition',
-        'value'   => $today,
-        'compare' => '>=',
+        'relation' => 'AND',
+        array(
+          'key'     => 'date_debut_evenement',
+          'value'   => $today,
+          'compare' => '<=',
+        ),
+        array(
+          'key' => 'date_fin_evenement',
+          'value'   => $today,
+          'compare' => '>=',
+        ),
       ),
     ),
-    'orderby' => 'date_debut_exposition',
+    'orderby' => array('date_debut_exposition', 'date_debut_evenement'),
     'order'   => 'ASC',
   );
   return array($avenir, $enCours, $archive);
--- template-parts/article.php
+++ template-parts/article.php
@@ -1,14 +1,43 @@
 
 <?php echo '<article class="row"name="'.$post->post_name.'">' ?>
 
- <section class="thePostText francais col-xs-offset-3 col-xs-20 col-sm-offset-3 col-sm-20 col-lg-offset-1 col-lg-9">
+  <section class="thePostText francais col-xs-offset-3 col-xs-20 col-sm-offset-3 col-sm-20 col-lg-offset-1 col-lg-9">
     <header >
       <h1 class="titre"><?php echo get_the_title(); ?></h1>
       <h2 class="subtitle"><?php echo get_field("sous-titre"); ?></h2>
+      <?php
+
+      if(get_post_type() === 'evenement'){
+        echo "<h2 class='evenement-type'>Évènement</h2>";
+      }
+      ?>
       <h3 class="temps"> du
-        <time ><?php echo templateFormatDate(get_post_meta( get_the_ID(), 'date_debut_exposition', true )); ?></time>
+        <time >
+          <?php
+          $dateDebutExpo = get_post_meta( get_the_ID(), 'date_debut_exposition', true );
+          $dateDebutEven = get_post_meta( get_the_ID(), 'date_debut_evenement', true );
+
+          if($dateDebutExpo){
+            echo templateFormatDate($dateDebutExpo);
+          }elseif ($dateDebutEven) {
+            echo templateFormatDate($dateDebutEven);
+          }
+          ?>
+        </time>
         au
-        <time ><?php echo templateFormatDate(get_post_meta( get_the_ID(), 'date_fin_exposition', true )); ?></time>
+        <time >
+          <?php
+          $dateFinExpo = get_post_meta( get_the_ID(), 'date_fin_exposition', true );
+          $dateFinEven = get_post_meta( get_the_ID(), 'date_fin_evenement', true );
+
+          if($dateFinExpo){
+            echo templateFormatDate($dateFinExpo);
+          }elseif ($dateFinEven) {
+            echo templateFormatDate($dateFinEven);
+          }
+          // echo templateFormatDate(get_post_meta( get_the_ID(), 'date_fin_exposition', true ));
+          ?>
+        </time>
       </h3>
     </header>
     <main>
@@ -16,44 +45,44 @@
     </main>
   </section>
 
-<?php if( have_rows('fichiers_media') ): ?>
-  <section class="thePostImages col-xs-offset-3 col-xs-20 col-sm-offset-10 col-sm-13 col-xs-24 col-sm-offset-3 col-sm-8 col-lg-offset-2 col-lg-13" data-spy="affix" data-offset-top="205">
-    <!-- <div class='img-slider-control'> -->
+  <?php if( have_rows('fichiers_media') ): ?>
+    <section class="thePostImages col-xs-offset-3 col-xs-20 col-sm-offset-10 col-sm-13 col-xs-24 col-sm-offset-3 col-sm-8 col-lg-offset-2 col-lg-13" data-spy="affix" data-offset-top="205">
+      <!-- <div class='img-slider-control'> -->
 
-    <!-- </div> -->
+      <!-- </div> -->
 
-    <div class="imgGal cols-xs-26">
-      <div class='img-prev-control' >
+      <div class="imgGal cols-xs-26">
+        <div class='img-prev-control' >
 
-      </div>
-      <div class='img-next-control'>
+        </div>
+        <div class='img-next-control'>
 
-      </div>
-      <?php
-      // $gal = get_post_meta(get_the_ID(),'gallery_data',true);
-      // foreach ($gal['image_url'] as $img) {
-      //   echo "<figure>";
-      //   echo "<img  src='".$img."' />";
-      //   echo "<figcaption></figcaption></figure>";
-      // }
-      ?>
-      <div class="swiper-wrapper">
-
+        </div>
         <?php
-        // loop through the rows of data
-        while ( have_rows('fichiers_media') ) : the_row();
-        $media = get_sub_field('media_file');
-        $decription = get_sub_field('description_media');
+        // $gal = get_post_meta(get_the_ID(),'gallery_data',true);
+        // foreach ($gal['image_url'] as $img) {
+        //   echo "<figure>";
+        //   echo "<img  src='".$img."' />";
+        //   echo "<figcaption></figcaption></figure>";
+        // }
+        ?>
+        <div class="swiper-wrapper">
 
-        echo '<div class="swiper-slide col-xs-26">';
-        echo '<img src="'.$media['url'].'"/>';
-        echo '<h2>'.$decription.'</h2>';
-        echo '</div>';
+          <?php
+          // loop through the rows of data
+          while ( have_rows('fichiers_media') ) : the_row();
+          $media = get_sub_field('media_file');
+          $decription = get_sub_field('description_media');
 
-      endwhile;
-      ?>
+          echo '<div class="swiper-slide col-xs-26">';
+          echo '<img src="'.$media['url'].'"/>';
+          echo '<h2>'.$decription.'</h2>';
+          echo '</div>';
+
+        endwhile;
+        ?>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
 <?php endif; ?>
 </article>
