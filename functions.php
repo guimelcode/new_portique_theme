d112190e0a31a103e431f79b8f68ab73ad66bcd0
mathieu-roquet <rock-mat@hotmail.fr>
2017-10-04 00:05:15 +0200

    working on ACF map... WIP

--- functions.php
+++ functions.php
@@ -123,14 +123,12 @@ function menu(){
 	/* Google map API */
 
 
-add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+function my_acf_init() {
 
-
-function my_theme_add_scripts() {
-    if (is_page('nous-trouver')) {
-        wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key="XXXX', array(), '3', true );
-        wp_enqueue_script( 'google-map-init', get_template_directory_uri() . '/google-maps.js', array('google-map', 'jquery'), '0.1', true );
-    }
+	acf_update_setting('google_api_key', 'AIzaSyC_ctP4dL6ZT95ERMDzekn2lBCsT2yjtYw');
 }
 
-add_action( 'wp_enqueue_scripts', 'my_theme_add_scripts' );
+add_action('acf/init', 'my_acf_init');
+
+
+/* */
