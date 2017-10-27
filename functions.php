bcff9b507d657aa31516af93980d5723cd4081ef
guimelcode <guillaume.melennec@gmail.com>
2017-10-27 16:52:27 +0200

    Forgot GMap Key

--- functions.php
+++ functions.php
@@ -148,7 +148,7 @@ add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 
 function my_acf_init() {
 
-	acf_update_setting('google_api_key', 'xxx');
+	acf_update_setting('google_api_key', 'AIzaSyAdbsSXw7JAcw3F6jMzGMGEEZ4N8Cb_oto');
 }
 
 add_action('acf/init', 'my_acf_init');
